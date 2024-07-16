343c9fa0738cf7416b7100c4ab6fbce9f01e3ae3
krystianzun <44001786+krystianzun@users.noreply.github.com>
2024-07-16 10:48:15 +0100

    Update Now section

--- components/NowSection.tsx
+++ components/NowSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import illitMagnetic from '@/public/images/illit-magnetic.jpg';
+import lisaRockstar from '@/public/images/lisaRockstar.jpg';
 
 function NowSection() {
   return (
@@ -9,10 +9,10 @@ function NowSection() {
         <h3 className='pb-4 text-grey-800 font-medium'>Now,</h3>
         <p className='pb-4'>If not I&apos;m not designing, I like moving heavy weights, running and travelling (a lot, over 180 days last year).</p>
         <p className='pb-4 flex'>Listening to
-            <Image src={illitMagnetic} alt="" width={24} height={24} className='mx-2 rounded'/>
+            <Image src={lisaRockstar} alt="" width={24} height={24} className='mx-2 rounded'/>
              <span className='group text-grey-800'>
                 <span className='border-b-2 border-grey-300 hover:border-grey-800'>
-                    <a href="https://www.youtube.com/watch?v=Vk5-c_v4gMU" target="_blank" rel="noopener noreferrer">Magnetic - ILLIT</a>
+                    <a href="https://www.youtube.com/watch?v=hbcGx4MGUMg" target="_blank" rel="noopener noreferrer">LISA - Rockstar</a>
                 </span>
                 <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
             </span> on repeat
@@ -21,12 +21,13 @@ function NowSection() {
             <span className='group text-grey-800'>
                 <span>&nbsp;</span>
                 <span className='border-b-2 border-grey-300 hover:border-grey-800'>
-                    <a href="https://www.goodreads.com/book/show/56447987-the-millionaire-fast-lane" target="_blank" rel="noopener noreferrer">The Millionaire Fastlane by MJ DeMarco</a>
+                    <a href="https://noahkagan.com/mdwbook/" target="_blank" rel="noopener noreferrer">Million Dollar Weekend by Noah Kagan</a>
                 </span>
                 <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
             </span>
         </p>
-        <p className='pb-4 text-grey-500'>Preparing for a <span className='text-grey-800'>road trip around Croatia, Bosnia, and Montenegro</span></p>
+        <p className='pb-8 text-grey-500'>Preparing for a <span className='text-grey-800'>road trip around Croatia, Bosnia, and Montenegro</span></p>
+        <p className='text-sm text-grey-500'>Last update: 16.07.24</p>
     </section>
   )
 }
