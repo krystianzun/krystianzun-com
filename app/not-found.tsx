040a446a8fb7f9df7721fa35be74edc34e0581f0
krystianzun <44001786+krystianzun@users.noreply.github.com>
2024-07-30 22:36:15 +0100

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,11 +2,10 @@ import Link from "next/link";
 import React from "react";
 import Image from "next/image";
 import notfoundgif from "@/public/images/404gif.webp";
-import lisaRockstar from "@/public/images/lisaRockstar.jpg";
 
 const NotFoundPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen m-6">
+    <div className="flex flex-col items-center justify-center h-dvh m-6">
       <div className="flex flex-col gap-2">
         <Image src={notfoundgif} alt="" width={400} height={250} className="" />
         <Link href="/" className="underline underline-offset-4">
