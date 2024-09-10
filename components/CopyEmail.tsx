11eff25939ceb3a98cefb7bfe9c45be108bd91d1
krystianzun <44001786+krystianzun@users.noreply.github.com>
2024-09-10 10:38:50 +0100

    Update

--- components/CopyEmail.tsx
+++ components/CopyEmail.tsx
@@ -1,45 +1,48 @@
 "use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const CopyEmail = () => {
-    const [copyStatus, setCopyStatus] = useState('');
-    const [isHovering, setIsHovering] = useState(false);
-  
-    const copyToClipboard = () => {
-      const email = 'krystian.zun@gmail.com';
-      navigator.clipboard.writeText(email).then(() => {
-        setCopyStatus('Copied!');
+  const [copyStatus, setCopyStatus] = useState("");
+  const [isHovering, setIsHovering] = useState(false);
+
+  const copyToClipboard = () => {
+    const email = "krystian.zun@gmail.com";
+    navigator.clipboard
+      .writeText(email)
+      .then(() => {
+        setCopyStatus("Copied!");
         setIsHovering(false); // Hide the "Click to copy" message
-        setTimeout(() => setCopyStatus(''), 1000); // Reset after 1 seconds
-      }).catch(() => {
-        setCopyStatus('Failed to copy');
-        setTimeout(() => setCopyStatus(''), 1000);
+        setTimeout(() => setCopyStatus(""), 1000); // Reset after 1 seconds
+      })
+      .catch(() => {
+        setCopyStatus("Failed to copy");
+        setTimeout(() => setCopyStatus(""), 1000);
       });
-    };
-  
-    return (
-      <span className="relative inline-block group">
-        <span
-          className="border-b-2 border-gray-300 group-hover:border-gray-800 cursor-pointer font-medium transition-colors duration-200"
-          onClick={copyToClipboard}
-          onMouseEnter={() => !copyStatus && setIsHovering(true)}
-          onMouseLeave={() => setIsHovering(false)}
-        >
-          krystian.zun@gmail.com
-        </span>
-        {isHovering && !copyStatus && (
-          <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-0.5 px-2 py-1 text-white bg-gray-800 whitespace-nowrap">
-            Click to copy
-          </span>
-        )}
-        {copyStatus && (
-          <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-0.5 px-2 py-1 text-white bg-green-available whitespace-nowrap">
-            {copyStatus}
-          </span>
-        )}
+  };
+
+  return (
+    <span className="relative inline-block group">
+      <span
+        className="border-b-2 border-gray-300 group-hover:border-gray-800 cursor-pointer font-medium transition-colors duration-200"
+        onClick={copyToClipboard}
+        onMouseEnter={() => !copyStatus && setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+      >
+        krystian.zun@gmail.com
       </span>
-    );
-}
+      {isHovering && !copyStatus && (
+        <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-0.5 px-2 py-1 text-white bg-gray-800 whitespace-nowrap">
+          Click to copy
+        </span>
+      )}
+      {copyStatus && (
+        <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-0.5 px-2 py-1 text-white bg-green-available whitespace-nowrap">
+          {copyStatus}
+        </span>
+      )}
+    </span>
+  );
+};
 
 export default CopyEmail;
--- components/ProjectsSection.tsx
+++ components/ProjectsSection.tsx
@@ -1,84 +1,142 @@
-import React from 'react'
-import { useState } from 'react'
-import CopyEmail from './CopyEmail'
-import CopyText from './CopyText'
+import React from "react";
+import { useState } from "react";
+import CopyText from "./CopyText";
 
 function ProjectsSection() {
   return (
     <section>
-        <h3 className='pb-4 text-grey-800 font-medium'>Recently,</h3>
-        <div className='pb-8'>
-            <div className='mb-1 flex space-x-2'>
-                <h4 className='group text-grey-800'>
-                    <span className='border-b-2 border-grey-300 hover:border-grey-800 font-medium'>
-                        <a href="https://www.xrjobsboard.com/" target="_blank" rel="noopener noreferrer">XR Jobs Board</a>
-                    </span>
-                    <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
-                </h4>
-                <div className='px-2 py-0.5 bg-green-100 rounded-sm text-sm text-green-900 font-medium'>
-                    Currently Building
-                </div>
-            </div>
-            <p className='text-grey-500'>Stop wasting time searching through countless job portals. All XR jobs and opportunities in one place.</p>    
-        </div>  
-        <div className='pb-8'>
-            <h4 className='group text-grey-800 mb-1'>
-                <span className='border-b-2 border-grey-300 hover:border-grey-800 font-medium'>
-                    <a href="https://krystianzun.gumroad.com/l/xrdesignroadmap" target="_blank" rel="noopener noreferrer">XR Design Learning Roadmap</a>
-                </span>
-                <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
-            </h4>
-            <p className='text-grey-500'>Step-by-step, comprehensive guide to all essential skills to design extended realities.</p>    
-        </div>  
-        <div className='pb-8'>
-            <h4 className='group text-grey-800 mb-1'>
-                <span className='border-b-2 border-grey-300 hover:border-grey-800 font-medium'>
-                    <a href="https://discord.com/invite/eUKt8mFqnb" target="_blank" rel="noopener noreferrer">Design Futures</a>
-                </span>
-                <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
-            </h4>
-            <p className='text-grey-500'>Community for all into XR, Design, 3D, AI and generally emerging and creative tech with weekly hang outs, world hopping sessions and themed discussions.</p>    
-        </div> 
-        <div className='pb-8'>
-            <h4 className='group text-grey-800 mb-1'>
-                <span className='border-b-2 border-grey-300 hover:border-grey-800 font-medium'>
-                    <a href="https://lu.ma/touchsomegrass" target="_blank" rel="noopener noreferrer">Touch Some Grass*</a>
-                </span>
-                <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
-            </h4>
-            <p className='text-grey-500'>Fun, informal meet-up where we chat about XR and creative tech in the great outdoors, immersed in the best reality (nature) ٩( ✩&apos;ω&apos;✩ )و</p>    
-        </div> 
-        <div className='pb-8 text-grey-800'>
-            <p className='pb-4'>and 
-                <span className='group text-grey-800'>
-                    <span>&nbsp;</span>
-                    <span className='border-b-2 border-grey-300 hover:border-grey-800 font-medium'>
-                        <a href="https://krystianzun.substack.com/" target="_blank" rel="noopener noreferrer">subscribe to my newsletter</a>
-                    </span>
-                    <span className='text-grey-400 group-hover:text-grey-800 font-medium'>&#x2197;</span>
-                </span> where I share my learnings, ideas and links to anything I&apos;ve found interesting
-            </p>
-            <div>
-                {/* 
-                    place for input and button to subscribe 
-                */} 
-            </div>
+      <h3 className="pb-4 text-grey-800 font-medium">Recently,</h3>
+      <div className="pb-8">
+        <div className="mb-1 flex space-x-2">
+          <h4 className="group text-grey-800">
+            <span className="border-b-2 border-grey-300 hover:border-grey-800 font-medium">
+              <a
+                href="https://www.xrjobsboard.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                XR Jobs Board
+              </a>
+            </span>
+            <span className="text-grey-400 group-hover:text-grey-800 font-medium">
+              &#x2197;
+            </span>
+          </h4>
+          <div className="px-2 py-0.5 bg-green-100 rounded-sm text-sm text-green-900 font-medium">
+            Currently Building
+          </div>
         </div>
-        <p className='text-grey-800'>I love meeting new people, feel free to drop me a line at
-            <span className='group text-grey-800'>
-                <span>&nbsp;</span>
-                <span className='border-b-2 border-gray-300 hover:border-gray-800 cursor-pointer inline-block font-medium'>
-                    <CopyText 
-                            text="krystian.zun@gmail.com"
-                            displayText='krystian.zun@gmail.com'
-                            hoverMessage='Click to copy'
-                    />
-                </span>
-                </span> *:･ﾟ
+        <p className="text-grey-500">
+          Stop wasting time searching through countless job portals. All XR jobs
+          and opportunities in one place.
         </p>
-        <hr className="w-full h-px bg-gray-300 border-0 my-14"></hr>
+      </div>
+      <div className="pb-8">
+        <h4 className="group text-grey-800 mb-1">
+          <span className="border-b-2 border-grey-300 hover:border-grey-800 font-medium">
+            <a
+              href="https://krystianzun.gumroad.com/l/xrdesignroadmap"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              XR Design Learning Roadmap
+            </a>
+          </span>
+          <span className="text-grey-400 group-hover:text-grey-800 font-medium">
+            &#x2197;
+          </span>
+        </h4>
+        <p className="text-grey-500">
+          Step-by-step, comprehensive guide to all essential skills to design
+          extended realities.
+        </p>
+      </div>
+      <div className="pb-8">
+        <h4 className="group text-grey-800 mb-1">
+          <span className="border-b-2 border-grey-300 hover:border-grey-800 font-medium">
+            <a
+              href="https://discord.com/invite/eUKt8mFqnb"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Design Futures
+            </a>
+          </span>
+          <span className="text-grey-400 group-hover:text-grey-800 font-medium">
+            &#x2197;
+          </span>
+        </h4>
+        <p className="text-grey-500">
+          Community for all into XR, Design, 3D, AI and generally emerging and
+          creative tech with weekly hang outs, world hopping sessions and themed
+          discussions.
+        </p>
+      </div>
+      <div className="pb-8">
+        <h4 className="group text-grey-800 mb-1">
+          <span className="border-b-2 border-grey-300 hover:border-grey-800 font-medium">
+            <a
+              href="https://lu.ma/touchsomegrass"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Touch Some Grass*
+            </a>
+          </span>
+          <span className="text-grey-400 group-hover:text-grey-800 font-medium">
+            &#x2197;
+          </span>
+        </h4>
+        <p className="text-grey-500">
+          Fun, informal meet-up where we chat about XR and creative tech in the
+          great outdoors, immersed in the best reality (nature) ٩(
+          ✩&apos;ω&apos;✩ )و
+        </p>
+      </div>
+      <div className="pb-8 text-grey-800">
+        <p className="pb-4">
+          and
+          <span className="group text-grey-800">
+            <span>&nbsp;</span>
+            <span className="border-b-2 border-grey-300 hover:border-grey-800 font-medium">
+              <a
+                href="https://krystianzun.substack.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                subscribe to my newsletter
+              </a>
+            </span>
+            <span className="text-grey-400 group-hover:text-grey-800 font-medium">
+              &#x2197;
+            </span>
+          </span>{" "}
+          where I share my learnings, ideas and links to anything I&apos;ve
+          found interesting
+        </p>
+        <div>
+          {/* 
+                    place for input and button to subscribe 
+                */}
+        </div>
+      </div>
+      <p className="text-grey-800">
+        I love meeting new people, feel free to drop me a line at
+        <span className="group text-grey-800">
+          <span>&nbsp;</span>
+          <span className="border-b-2 border-gray-300 hover:border-gray-800 cursor-pointer inline-block font-medium">
+            <CopyText
+              text="krystian.zun@gmail.com"
+              displayText="krystian.zun@gmail.com"
+              hoverMessage="Click to copy"
+            />
+          </span>
+        </span>{" "}
+        *:･ﾟ
+      </p>
+      <hr className="w-full h-px bg-gray-300 border-0 my-14"></hr>
     </section>
-  )
+  );
 }
 
-export default ProjectsSection
+export default ProjectsSection;
