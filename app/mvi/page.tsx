dbf65a1bf2dce37f384a0a50c4d0899fa07cd1d5
krystianzun <44001786+krystianzun@users.noreply.github.com>
2025-02-25 18:15:10 +0000

    add lowercase just for vibes

--- app/mvi/page.tsx
+++ app/mvi/page.tsx
@@ -3,10 +3,10 @@ import React from "react";
 const MviPage = () => {
   return (
     <main className="mb-32 mt-24">
-      <section className="m-auto flex max-w-md flex-col gap-8 text-primary">
-        <div className="flex flex-col gap-4">
+      <section className="m-auto flex max-w-md flex-col gap-12 text-primary text-lg lowercase">
+        <div className="flex flex-col gap-8">
           <h1 className="text-2xl font-medium">
-            Validate your idea by end of the week{" "}
+            validate your idea by end of the week
           </h1>
           <p>
             Too often, founders jump straight into building an MVP that takes
@@ -15,6 +15,24 @@ const MviPage = () => {
           </p>
           <p>
             There’s a faster and better way to see if your idea has real demand.
+          </p>
+        </div>
+        <div className="w-full flex justify-center text-base flex-col items-center gap-2">
+          <button className="bg-black cursor-pointer text-white p-4 w-full max-w-sm">
+            validate&nbsp;&nbsp;&nbsp;( $1499 )
+          </button>
+          <p className="text-grey-600 text-sm">5 – 7 days</p>
+        </div>
+        <div className="flex flex-col gap-8">
+          <h3 className="text-xl font-medium">How?</h3>
+          <p>
+            I partner with founders and startup and offer a shortcut to learn
+            without building and launching.
+          </p>
+          <p>
+            I&apos;ll help you validate your idea and answer business questions
+            by designing, prototyping, and testing ideas with your real
+            customers.
           </p>
         </div>
       </section>
