404c3ab9e6e003c93b3168bc5dbc9e7b3bd778c5
krystianzun <44001786+krystianzun@users.noreply.github.com>
2025-02-25 17:59:33 +0000

    Update page.tsx

--- app/mvi/page.tsx
+++ app/mvi/page.tsx
@@ -3,42 +3,20 @@ import React from "react";
 const MviPage = () => {
   return (
     <main className="mb-32 mt-24">
-      <section className="m-auto flex max-w-md flex-col gap-16 text-primary">
-        <h1 className="text-2xl font-medium">About</h1>
+      <section className="m-auto flex max-w-md flex-col gap-8 text-primary">
         <div className="flex flex-col gap-4">
+          <h1 className="text-2xl font-medium">
+            Validate your idea by end of the week{" "}
+          </h1>
           <p>
-            Hey, I&apos;m Krystian, a product designer specializing in XR and
-            creative technology. Over the past several months, I&apos;ve been
-            building tools and initiatives to support the XR community.
+            Too often, founders jump straight into building an MVP that takes
+            weeks and costs thousands of $$$, only to find out it&apos;s not
+            what users want.
           </p>
           <p>
-            One recurring challenge I&apos;ve encountered, and heard from
-            others, is the difficulty in finding jobs and opportunities within
-            the XR space. This often comes from a fragmented ecosystem of job
-            platforms, inconsistent terminology, ambiguous position labels, and
-            a lack of tailored curation for this unique field.
-          </p>
-          <p>
-            That&apos;s why I decided to build XR Jobs Board—a community-focused
-            central hub for job listings and opportunities for designers,
-            developers, and producers.
-          </p>
-          <p>
-            My vision is for XR Jobs Board to become the definitive go-to place
-            for anyone navigating a career in XR and immersive technologies.
-          </p>
-          <p>
-            Whether you&apos;re a seasoned professional looking to advance your
-            career, a newcomer eager to break in, or simply curious about the
-            possibilities in extended reality, XR Jobs Board is the right place
-            for you.
+            There’s a faster and better way to see if your idea has real demand.
           </p>
         </div>
-        <p>Sign up for the list and let&apos;s get going! ✧ ─=≡Σ((( つ•̀ω•́)つ</p>
-        <p>
-          With love,
-          <br /> Krystian
-        </p>
       </section>
     </main>
   );
