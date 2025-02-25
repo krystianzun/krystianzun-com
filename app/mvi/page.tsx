7a6fcefd63af7982e03f72530a714c32cc586848
krystianzun <44001786+krystianzun@users.noreply.github.com>
2025-02-25 18:48:42 +0000

    add all the first draft copy

--- app/mvi/page.tsx
+++ app/mvi/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Verified from "@/components/icons/Verified";
+import Cross from "@/components/icons/Cross";
 
 const MviPage = () => {
   return (
-    <main className="mb-32 mt-24 text-grey-800">
+    <main className="mb-48 mt-24 text-grey-800">
       <section className="m-auto flex max-w-md flex-col gap-12 text-primary text-lg lowercase">
         <div className="flex flex-col gap-6 ">
           <h1 className="text-2xl font-medium text-black">
@@ -77,6 +78,80 @@ const MviPage = () => {
             Plus, we can help you kickstart a community of early
             adopters—turning your first testers into your future customers.
           </p>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-xl font-medium text-black">Who I Am</h3>
+          <p>
+            hey, I&apos;m krystian zun and I&apos;ve spent the last 7 years
+            working at early-stage startups and spinning new initiatives within
+            companies.
+          </p>
+          <p>
+            I&apos;ve seen firsthand weeks wasted on design and development
+            based on whim and hunch, even though it could have been very easily
+            avoided.
+          </p>
+          <p>
+            before I launched XR Jobs Board, I shared around a scrappy video and
+            landing page. in less than 48 hours I got 400+ sign-ups, and 120+
+            feedback submissions.
+          </p>
+          <p>
+            I love coming up with ways to validate and test new ideas and now I
+            want to do it with you
+          </p>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-xl font-medium text-black">Who Is This For</h3>
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-start transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Early-stage founders who want to test demand before investing in
+              an MVP
+            </li>
+            <li className="flex items-center transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Product teams exploring new features or markets
+            </li>
+            <li className="flex items-start transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Anyone looking to validate an idea quickly, with minimal upfront
+              cost
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-xl font-medium text-black">Who Isn’t This For</h3>
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center transition-all duration-300 ease-in-out hover:ml-2">
+              <Cross className="mr-2 mt-0.5 text-red-500" size={24} />
+              Founders looking for full MVP development
+            </li>
+            <li className="flex items-center transition-all duration-300 ease-in-out hover:ml-2">
+              <Cross className="mr-2 mt-0.5 text-red-500" size={24} />
+              Teams who prefer to build without user validation
+            </li>
+            <li className="flex items-start transition-all duration-300 ease-in-out hover:ml-2">
+              <Cross className="mr-2 mt-0.5 text-red-500" size={24} />
+              Projects with no flexibility to iterate based on feedback
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-xl font-medium text-black">
+            Let’s validate your idea
+          </h3>
+          <p>
+            If you&apos;re ready to test your idea before investing in
+            development, get in touch. I&apos;ll help you find out if your
+            concept has traction—without the guesswork.
+          </p>
+        </div>
+        <div className="w-full flex justify-center text-base flex-col items-center gap-2">
+          <button className="bg-black cursor-pointer text-white p-4 w-full max-w-sm">
+            validate&nbsp;&nbsp;&nbsp;( $1499 )
+          </button>
+          <p className="text-grey-600 text-sm">5 – 7 days</p>
         </div>
       </section>
     </main>
