d34b839830c6bd09841dde1046820f618c1bd417
krystianzun <44001786+krystianzun@users.noreply.github.com>
2025-02-25 18:30:18 +0000

    Update page.tsx

--- app/mvi/page.tsx
+++ app/mvi/page.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Verified from "@/components/icons/Verified";
 
 const MviPage = () => {
   return (
-    <main className="mb-32 mt-24">
+    <main className="mb-32 mt-24 text-grey-800">
       <section className="m-auto flex max-w-md flex-col gap-12 text-primary text-lg lowercase">
-        <div className="flex flex-col gap-6">
-          <h1 className="text-2xl font-medium">
+        <div className="flex flex-col gap-6 ">
+          <h1 className="text-2xl font-medium text-black">
             validate your idea by end of the week
           </h1>
           <p>
@@ -24,7 +25,7 @@ const MviPage = () => {
           <p className="text-grey-600 text-sm">5 – 7 days</p>
         </div>
         <div className="flex flex-col gap-6">
-          <h3 className="text-xl font-medium">How?</h3>
+          <h3 className="text-xl font-medium text-black">How?</h3>
           <p>
             I partner with founders and startup and offer a shortcut to learn
             without building and launching.
@@ -36,9 +37,46 @@ const MviPage = () => {
           </p>
         </div>
         <div className="flex flex-col gap-6">
-          <h3 className="text-xl font-medium">
+          <h3 className="text-xl font-medium text-black">
             In just a few days, we&apos;ll:
           </h3>
+          <ul className="flex flex-col gap-3">
+            <li className="flex items-center transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Define clear success metrics and hypotheses
+            </li>
+            <li className="flex items-center transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Build a rapid prototype tailored to your needs
+            </li>
+            <li className="flex items-start transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              Test with real users to gauge trust, attention, and willingness to
+              pay
+            </li>
+            <li className="flex items-start transition-all duration-300 ease-in-out hover:ml-2">
+              <Verified className="mr-2 mt-0.5 text-green-500" size={24} />
+              <span className="flex flex-col">
+                Deliver actionable insights and a clear next step <br />
+                <span className="text-grey-500 text">
+                  (whether that’s refining your concept or building your MVP
+                  with confidence)
+                </span>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-xl font-medium text-black">Why This Works</h3>
+          <p>Building an MVP is expensive (time & money).</p>
+          <p>
+            By testing demand upfront, you avoid weeks of wasted development and
+            focus on what users actually want.
+          </p>
+          <p>
+            Plus, we can help you kickstart a community of early
+            adopters—turning your first testers into your future customers.
+          </p>
         </div>
       </section>
     </main>
