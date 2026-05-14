ac599423478d6b0be7384153daf2f9205234dcba
krystianzun <44001786+krystianzun@users.noreply.github.com>
2026-05-14 08:54:25 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -12,8 +12,13 @@ const links = [
 function Footer() {
   return (
     <footer className="pb-8">
-      <div className="text-muted mt-14">
-        ©{new Date().getFullYear()} Krystian Zun, London{" "}
+      <div className="flex items-center justify-between mt-14">
+        <div className="text-muted">
+          ©{new Date().getFullYear()} London, (ﾉ◕ヮ◕)ﾉ *:･ﾟ
+        </div>
+        <span className="text-muted bg-gray-100 rounded-full px-2 py-1 text-xs">
+          v1.0
+        </span>
       </div>
     </footer>
   );
